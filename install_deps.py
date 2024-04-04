5aeaf393bc6d6b0c83d95db53052e191f17a440c
Dominic Sudy <dominic.sudy@bosch.com>
2024-04-04 11:32:32 +0200

    Add missing files

--- install_deps.py
+++ install_deps.py
@@ -12,6 +12,11 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+"""
+Install all software depenencies of the given Velocitas project via a
+simple command line interface.
+"""
+
 import subprocess
 import os
 from argparse import ArgumentParser
@@ -28,32 +33,65 @@ def safe_get_workspace_dir() -> str:
         return "."
 
 
+def get_profile_name(arch: str, build_variant: str) -> str:
+    """Return the Conan profile name for the given `arch` and
+    `build_variant`.
+
+    Args:
+        arch (str): The architecture of the profile.
+        build_variant (str): The build variant (debug or release).
+
+    Returns:
+        str: The Conan profile name.
+    """
+    return f"linux_{arch}_{build_variant}"
+
 
 def install_deps_via_conan(
-    build_arch: str, is_debug: bool = False, build_all_deps: bool = False
+    build_arch: str, host_arch: str, is_debug: bool = False, build_all_deps: bool = False
 ) -> None:
     build_variant = "debug" if is_debug else "release"
-    profile_filename = f"linux_{build_arch}_{build_variant}"
+
+    profile_build_path = (
+        Path(__file__)
+        .absolute()
+        .parent.joinpath(".conan", "profiles", get_profile_name(build_arch, build_variant))
+    )
+
     profile_host_path = (
         Path(__file__)
         .absolute()
-        .parent.joinpath(".conan", "profiles", profile_filename)
+        .parent.joinpath(".conan", "profiles", get_profile_name(host_arch, build_variant))
     )
 
     build_folder = os.path.join(safe_get_workspace_dir(), "build")
     os.makedirs(build_folder, exist_ok=True)
 
     deps_to_build = "missing" if not build_all_deps else "*"
+
+    toolchain=f"/usr/bin/{host_arch}-linux-gnu"
+    build_host=f"{host_arch}-linux-gnu"
+    cc_compiler="gcc"
+    cxx_compiler="g++"
+
+    os.environ["CONAN_CMAKE_FIND_ROOT_PATH"] = toolchain
+    os.environ["CONAN_CMAKE_SYSROOT"] = toolchain
+    os.environ["CC"] = f"{build_host}-{cc_compiler}"
+    os.environ["CXX"] = f"{build_host}-{cxx_compiler}"
+
     subprocess.check_call(
         [
             "conan",
             "install",
             "-pr:h",
             profile_host_path,
+            "-pr:b",
+            profile_build_path,
             "--build",
             deps_to_build,
             "..",
         ],
+        env=os.environ,
         cwd=build_folder,
     )
 
@@ -78,12 +116,22 @@ def cli() -> None:
         action="store_true",
         help="Forces all dependencies to be rebuild from source.",
     )
+    argument_parser.add_argument(
+        "-x",
+        "--cross",
+        action="store",
+        help="Enables cross-compilation to the defined target architecture."
+    )
     args = argument_parser.parse_args()
 
-    host_arch = subprocess.check_output(["arch"], encoding="utf-8").strip()
+    build_arch = subprocess.check_output(["arch"], encoding="utf-8").strip()
+    host_arch = args.cross
+
+    if host_arch is None:
+        host_arch = build_arch
 
     install_deps_via_conan(
-        host_arch, args.debug or not args.release, args.build_all_deps
+        build_arch, host_arch, args.debug or not args.release, args.build_all_deps
     )
 
 
